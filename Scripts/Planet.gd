2db08e22537206d9b200dbbc84584603b86b2395
MaxDahlen <max.m.dahlen@gmail.com>
2026-06-21 22:36:25 +0200

    Raderade onödiga foldern

--- Scripts/Planet.gd
+++ Scripts/Planet.gd
@@ -1,15 +1,12 @@
 extends CharacterBody2D
 
+
 var time = 0
-var specific_rotation_speed = 1
-var rotation_distance
 
+var rotation_distance = 200
 
-func _ready(): #Ser till att de roterar vid rätt avstånd
-	rotation_distance = position.x
-	
 func _physics_process(delta: float) -> void:
-	time += GlobalVariables.Universe_Speed * delta * specific_rotation_speed #styr hur snabbt systemet rör sig
+	time += GlobalVariables.Universe_Speed * delta #styr hur snabbt systemet rör sig
 	position.x = rotation_distance*cos(time)
 	position.y = rotation_distance*sin(time)
 
