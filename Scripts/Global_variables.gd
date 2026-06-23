677715cc054593a45de1cb7793cc85543686efde
zackarias faarinen <zackarias.faarinen@gmail.com>
2026-06-23 12:59:32 +0200

    fixade planet positioner, planet 1 och 2 tillagda

--- Scripts/Global_variables.gd
+++ Scripts/Global_variables.gd
@@ -2,4 +2,4 @@ extends Node
 
 
 var Universe_Speed = 1 #Globala variablen som styr rotations hastighet
-const Universum_max = 1
+const Universum_max = 0.2
--- Scripts/Planet.gd
+++ Scripts/Planet.gd
@@ -1,17 +1,19 @@
 extends CharacterBody2D
 
 var time = 0
-var specific_rotation_speed = 1
-var rotation_distance
 
+var rotation_distance
+var rotation_position_x
+var rotation_position_y
 
 func _ready(): #Ser till att de roterar vid rätt avstånd
-	rotation_distance = position.x
-	
+	rotation_distance = sqrt(position.x**2 + position.y**2)
+	rotation_position_x = cos(position.x / rotation_distance)
+	rotation_position_y = cos(position.y / rotation_distance)
 func _physics_process(delta: float) -> void:
-	time += GlobalVariables.Universe_Speed * delta * specific_rotation_speed #styr hur snabbt systemet rör sig
-	position.x = rotation_distance*cos(time)
-	position.y = rotation_distance*sin(time)
+	time += GlobalVariables.Universe_Speed * delta / (rotation_distance/1000) #styr hur snabbt systemet rör sig
+	position.x = rotation_distance*cos(time + rotation_position_x)
+	position.y = rotation_distance*sin(time + rotation_position_y)
 
 	
 	
